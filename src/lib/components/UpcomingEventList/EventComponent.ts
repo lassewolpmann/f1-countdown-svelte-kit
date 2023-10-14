11ba92fd271b4a45aeb65ca07b1fefb39127dc8e
Lasse Wolpmann <lassewolpmann@outlook.de>
2023-10-14 20:53:55 +0300

    EventComponent - optimized class

--- src/lib/components/UpcomingEventList/EventComponent.ts
+++ src/lib/components/UpcomingEventList/EventComponent.ts
@@ -3,13 +3,15 @@ import type { Event } from "$lib/types/Data"
 export class EventComponent {
     event: Event
     locationURL: string;
-    sessionNames: string[];
     eventName: string;
+
+    sessionNames: string[];
+
     sessionsHidden: boolean;
 
     sessionsDateTime: string[];
-    sessionDates: string[];
-    sessionTimes: string[];
+    sessionDates: (string | undefined)[];
+    sessionTimes: (string | undefined)[];
 
     raceDate: string | undefined;
     raceTime: string | undefined;
@@ -18,20 +20,14 @@ export class EventComponent {
         this.event = event;
         this.locationURL = `https://www.google.com/maps/place/${this.event.latitude},${this.event.longitude}`;
         this.eventName = this.parseName(this.event.name);
-        this.sessionNames = Object.keys(this.event.sessions);
+
+        this.sessionNames = Object.keys(this.event.sessions).map(this.toUpper);
+
         this.sessionsHidden = true;
 
         this.sessionsDateTime = Object.values(this.event.sessions);
-        this.sessionDates = [];
-        this.sessionTimes = [];
-
-        for (const sessionDateTime of this.sessionsDateTime) {
-            const sessionDate = this.parseDate(sessionDateTime);
-            const sessionTime = this.parseTime(sessionDateTime);
-
-            if (sessionDate) this.sessionDates.push(sessionDate);
-            if (sessionTime) this.sessionTimes.push(sessionTime);
-        }
+        this.sessionDates = this.sessionsDateTime.map(this.parseDate);
+        this.sessionTimes = this.sessionsDateTime.map(this.parseTime);
 
         this.raceDate = this.sessionDates.at(-1);
         this.raceTime = this.sessionTimes.at(-1);
@@ -45,25 +41,21 @@ export class EventComponent {
         }
     }
 
-    parseDate = (sessionDate: string | undefined) => {
-        if (sessionDate) {
-            return new Date(sessionDate).toLocaleString(undefined, {
-                month: 'short',
-                day: '2-digit'
-            })
-        } else {
-            return undefined
-        }
+    parseDate = (sessionDate: string) => {
+        return new Date(sessionDate).toLocaleString(undefined, {
+            month: 'short',
+            day: '2-digit'
+        })
     }
 
-    parseTime = (sessionDate: string | undefined) => {
-        if (sessionDate) {
-            return new Date(sessionDate).toLocaleString(undefined, {
-                hour: '2-digit',
-                minute: '2-digit'
-            })
-        } else {
-            return undefined
-        }
+    parseTime = (sessionDate: string) => {
+        return new Date(sessionDate).toLocaleString(undefined, {
+            hour: '2-digit',
+            minute: '2-digit'
+        })
+    }
+
+    toUpper = (name: string) => {
+        return name.toUpperCase()
     }
 }
