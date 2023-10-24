7168f451d661d9df474d977fc1949d91282a0d01
Lasse Wolpmann <lassewolpmann@outlook.de>
2023-10-24 12:51:16 +0300

    Changes: - updated meta description tag - upgraded dependencies

--- src/lib/components/MetaDescription/MetaDescription.svelte
+++ src/lib/components/MetaDescription/MetaDescription.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
     // Type imports
     import type { AllSeriesData } from "$lib/types/Data";
-    import { getSessionNames, getNextEventName } from "$lib/components/MetaDescription/MetaDescription";
+    import { MetaDescription } from "$lib/components/MetaDescription/MetaDescription";
 
     export let seriesData: AllSeriesData, seriesList: string[];
+
+    const metaDescription = new MetaDescription(seriesList, seriesData);
 </script>
 
 <svelte:head>
-    {#if seriesList}
-        {#each seriesList as series}
-            <meta name="description" content="Countdown to the {new Date().getFullYear()} {series.toUpperCase()} {getNextEventName(seriesData, series)} Grand Prix {getSessionNames(seriesData, series)}">
-        {/each}
-    {/if}
+    <meta name="description" content="{metaDescription.descriptionString}">
 </svelte:head>
